43a0214a67e29bb54c3b1e15bf82dff8d9dc3aee
EngArsany <arsanyo2003@gmail.com>
2026-04-30 01:51:05 +0300

    Initialize Bridge-Server Project

---
+++ backend/MarketplaceBridgeServer/src/main/java/com/marketplace/bridge/WebSocketServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.marketplace.bridge;
+
+/**
+ *
+ * @author Arsany
+ */
+public class WebSocketServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
