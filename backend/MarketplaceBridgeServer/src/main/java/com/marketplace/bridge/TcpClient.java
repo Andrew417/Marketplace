42ddf0b9b7c13b81df1639c8440cb28dd1a23a65
EngArsany <arsanyo2003@gmail.com>
2026-05-08 22:45:07 +0300

    Bridge: Implement TcpClient

--- backend/MarketplaceBridgeServer/src/main/java/com/marketplace/bridge/TcpClient.java
+++ backend/MarketplaceBridgeServer/src/main/java/com/marketplace/bridge/TcpClient.java
@@ -4,10 +4,31 @@
  */
 package com.marketplace.bridge;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  *
  * @author Arsany
  */
 public class TcpClient {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
     
+    public void connect(String host, int port) throws IOException{
+        this.socket = new Socket(host, port);
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+    public void send(String message){
+        out.print(message);
+    }
+    public String receive() throws IOException{
+        return in.readLine();
+    }
+    public void close() throws IOException{
+        if (socket!= null)
+            socket.close();
+    }
 }
